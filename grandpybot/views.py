dfe16f6f1053c6fbd7946d82ed19427d5c00726b
Gotha01 <72265241+Gotha01@users.noreply.github.com>
2021-12-01 18:08:44 +0100

    views_modification

--- grandpybot/views.py
+++ grandpybot/views.py
@@ -9,4 +9,4 @@ app = Flask(__name__, static_folder=STATIC_DIR, template_folder=TEMPLATE_DIR)
 
 @app.route('/')
 def base():
-    return render_template("home.html", title='GrandPyBot', banniere="../static/img/banniere_papy_acceuil.png")
+    return render_template("base.html", title='GrandPyBot', banniere="../static/img/banniere_papy_acceuil.png")
