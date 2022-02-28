f3fc60d97dee498ba9772a83cef67b0ca05de50a
Gotha01 <72265241+Gotha01@users.noreply.github.com>
2022-02-28 11:03:11 +0100

    deploy

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ from grandpybot.views import app
 
 if __name__ == "__main__":
     app.debug=True
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5400))
     app.run(debug=True, host='0.0.0.0', port=port)
